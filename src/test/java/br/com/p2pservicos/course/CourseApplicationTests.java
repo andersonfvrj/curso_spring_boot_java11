15c684fe14ccb786a9664d0bcf0d07cdb18f1b9d
Anderson <anderson.fv.rj@gmail.com>
2020-12-28 19:33:07 -0300

    Projeto criado

---
+++ src/test/java/br/com/p2pservicos/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.p2pservicos.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
