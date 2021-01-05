2737d5551433d7d323fd00dff0714c4d783db442
Anderson <anderson.fv.rj@gmail.com>
2021-01-05 17:34:31 -0300

    Entidade payment

--- src/main/java/br/com/p2pservicos/course/config/TestConfig.java
+++ src/main/java/br/com/p2pservicos/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.p2pservicos.course.entities.Category;
 import br.com.p2pservicos.course.entities.Order;
 import br.com.p2pservicos.course.entities.OrderItem;
+import br.com.p2pservicos.course.entities.Payment;
 import br.com.p2pservicos.course.entities.Product;
 import br.com.p2pservicos.course.entities.User;
 import br.com.p2pservicos.course.entities.enums.OrderStatus;
@@ -88,6 +89,11 @@ public class TestConfig implements CommandLineRunner { //Macete para criar os da
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		//Para salvar objetos que se relacionam de forma um para um, chama o metodo da classe principal
+		o1.setPayment(pay1);
+		orderRepository.save(o1); //Manda o JPA salvar o objeto
 		
 		
 	}
